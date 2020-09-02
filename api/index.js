5ecfeea627381326fdcad5edaa3ec5bfa4403f62
Jorge Hernández <lesthack@gmail.com>
2020-09-02 13:30:21 -0500

    Testing-4

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
+const axios = require("axios");
+
+function request(data, headers) {
+  return axios({
+    url: "https://api.github.com/graphql",
+    method: "post",
+    headers,
+    data,
+  });
+}
+
 export default (req, res) => {
   res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ name: 'John Doe' }))
+  res.setHeader("Content-Type", "image/svg+xml");
+
+  try{
+
+  }
+  catch(err){
+    return res.send(renderError(err.message, err.secondaryMessage));
+  }
 }
